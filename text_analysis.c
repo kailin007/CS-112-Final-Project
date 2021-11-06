c847424f90929770fa7bed85b324af20bdfa38fa
Kailin Zhang <kailin@comp112-04.eecs.tufts.edu>
2021-11-06 17:18:09 -0400

    restore AnalyzeRequest

--- text_analysis.c
+++ text_analysis.c
@@ -75,3 +75,46 @@ struct RequestInfo AnalyzeRequest (char *text){
 
     return requestInfo;
 }
+
+
+struct ResponseInfo AnalyzeResponse (char* text){
+    struct ResponseInfo responseInfo = {0, 0, 0};
+    char *buffer, *p1, *p2;
+    int i = 0,j = 0;
+
+    buffer = text;
+    if((p1 = strstr(buffer, "\r\n\r\n")) != NULL){
+        responseInfo.headerLength = p1 - buffer + 4; // + 1 = + \n
+        printf("headerLength: %d\n", responseInfo.headerLength);
+    }
+
+    if((p1 = strstr(buffer, "Cache-Control")) != NULL){
+        // printf( "Has Cache-Control\n");
+        responseInfo.hasCacheControl = 1;
+        if((p1 = strstr(buffer, "max-age")) != NULL){
+            // printf( "Has max-age\n");
+            responseInfo.needCache = 1;
+            p1 = strchr(p1, '=');
+            p2 = strchr(p1, '\r');
+            char maxageStr[10];
+            bzero(maxageStr, 10);
+            memcpy(maxageStr, p1 + 1, p2 - p1 - 1);
+            maxageStr[p2 - p1 - 1] = '\0';
+            responseInfo.maxAge = atoi(maxageStr);
+            printf("responseInfo.maxAge: %d\n", responseInfo.maxAge);
+        }
+    }
+
+    if((p1 = strstr(buffer, "Content-Length")) != NULL){
+        p1 = strchr(p1, ' ');
+        p2 = strchr(p1, '\r');
+        char contentLengthStr[10];
+        bzero(contentLengthStr, 10);
+        memcpy(contentLengthStr, p1 + 1, p2 - p1 - 1);
+        contentLengthStr[p2 - p1 - 1] = '\0';
+        responseInfo.contentLength = atoi(contentLengthStr);
+        printf("Content-Length: %d\n", responseInfo.contentLength);
+    }
+
+    return responseInfo;
+}
